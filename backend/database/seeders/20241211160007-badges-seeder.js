49642189a8b7101d11e47cb20f45ef4c3eadd715
andrnnnn <andrianjack1@gmail.com>
2024-12-12 09:18:50 +0700

    Update url image

--- backend/database/seeders/20241211160007-badges-seeder.js
+++ backend/database/seeders/20241211160007-badges-seeder.js
@@ -7,35 +7,35 @@ module.exports = {
       {
         name: 'First Steps',
         description: 'Menyelesaikan kursus pertama dengan nilai minimal B',
-        image_url: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+        image_url: 'https://cdn-icons-png.flaticon.com/128/1435/1435688.png',
         requirement: 'Selesaikan 1 kursus dengan nilai minimal 80',
         created_at: new Date()
       },
       {
         name: 'Code Warrior',
         description: 'Menyelesaikan 5 project pemrograman dengan sempurna',
-        image_url: 'https://cdn-icons-png.flaticon.com/512/6941/6941697.png',
+        image_url: 'https://cdn-icons-png.flaticon.com/128/1435/1435703.png',
         requirement: 'Selesaikan 5 project coding dengan nilai 90+',
         created_at: new Date()
       },
       {
         name: 'Data Master',
         description: 'Mahir dalam analisis dan visualisasi data',
-        image_url: 'https://cdn-icons-png.flaticon.com/512/4337/4337972.png',
+        image_url: 'https://cdn-icons-png.flaticon.com/128/1435/1435697.png',
         requirement: 'Selesaikan semua kursus Data Science dengan nilai rata-rata 85+',
         created_at: new Date()
       },
       {
         name: 'Design Guru',
         description: 'Menciptakan desain UI/UX yang luar biasa',
-        image_url: 'https://cdn-icons-png.flaticon.com/512/1170/1170667.png',
+        image_url: 'https://cdn-icons-png.flaticon.com/128/1435/1435704.png',
         requirement: 'Selesaikan 3 project UI/UX dengan feedback positif',
         created_at: new Date()
       },
       {
         name: 'Perfect Attendance',
         description: 'Kehadiran 100% dalam satu kursus',
-        image_url: 'https://cdn-icons-png.flaticon.com/512/1312/1312304.png',
+        image_url: 'https://cdn-icons-png.flaticon.com/128/1435/1435683.png',
         requirement: 'Hadiri semua sesi dalam satu kursus tanpa absen',
         created_at: new Date()
       }
