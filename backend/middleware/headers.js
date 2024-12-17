54900e6dcfdef6bc99870b8f106917d83672fcfc
Koutsura <m.denny.trilisandi@gmail.com>
2024-12-17 15:10:18 +0700

    /

--- backend/middleware/headers.js
+++ backend/middleware/headers.js
@@ -7,7 +7,7 @@ const headersMiddleware = (req, res, next) => {
       "style-src 'self' https://cdnjs.cloudflare.com https://fonts.googleapis.com; " +
       "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com data:; " +  // Allow fonts from data: and trusted sources
       "img-src 'self' data: https: https://www.youtube.com; " +  // Allow images from YouTube (thumbnails)
-      "connect-src 'self' https://pintura.decadev.tech; " +  // Add your backend API URL here
+      "connect-src 'self' https://pintura.decadev.tech/; " +  // Add your backend API URL here
       "frame-src 'self' https://www.youtube.com; " +  // Allow embedding YouTube videos
       "frame-ancestors 'none';"
   );
