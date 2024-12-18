6fe60b0a197586968babf63e20a4415273fa0822
Koutsura <m.denny.trilisandi@gmail.com>
2024-12-18 11:26:13 +0700

    update cors

--- backend/middleware/validateHost.js
+++ backend/middleware/validateHost.js
@@ -1,13 +1,12 @@
 const validateHostMiddleware = (req, res, next) => {
-    const allowedHosts = ['localhost', '127.0.0.1', 'yourdomain.com'];
-  
-    if (!allowedHosts.includes(req.hostname)) {
-      console.warn(`Blocked request with invalid Host header: ${req.hostname}`);
-      return res.status(400).json({ message: 'Invalid Host header' });
-    }
-  
-    next();
-  };
-  
-  module.exports = validateHostMiddleware;
-  
+  const allowedHosts = ['localhost', '127.0.0.1', 'yourdomain.com', 'https://pintura.decadev.tech', 'pintura.decadev.tech'];
+
+  if (!allowedHosts.includes(req.hostname)) {
+    console.warn(`Blocked request with invalid Host header: ${req.hostname}`);
+    return res.status(400).json({ message: 'Invalid Host header' });
+  }
+
+  next();
+};
+
+module.exports = validateHostMiddleware;
